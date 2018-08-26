0a2725180c3d828bd9941ec726e7c696791cf014
silberpfeilv2 <40671874+silberpfeilv2@users.noreply.github.com>
2018-08-26 22:23:50 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,6 @@
+echo "# wel-kr-wlker" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/silberpfeilv2/wel-kr-wlker.git
+git push -u origin master
